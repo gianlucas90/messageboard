08ed5308242c0fed3c3e482a759a225394977bdc
Gianluca <gianlucazin1990@gmail.com>
2021-01-24 21:31:43 +0100

    tests passed locally

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,181 @@
 'use strict';
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+require('dotenv').config();
 
 module.exports = function (app) {
-  
-  app.route('/api/threads/:board');
-    
-  app.route('/api/replies/:board');
+  mongoose.connect(process.env.DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 
+  const db = mongoose.connection;
+  db.on('error', console.error.bind(console, 'connection error:'));
+
+  db.once('open', function () {
+    console.log('we are conncected to the db!');
+  });
+
+  // To delete all documents in the database everytime restart the server!
+  // db.dropDatabase();
+
+  /////////////////// Schemas
+  const threadSchema = new Schema({
+    board: { type: String, required: true },
+    text: { type: String, required: true },
+    created_on: {
+      type: Date,
+      default: Date.now,
+    },
+    bumped_on: {
+      type: Date,
+      default: Date.now,
+    },
+    reported: { type: Boolean, default: false },
+    delete_password: String,
+    replies: [
+      {
+        text: String,
+        created_on: {
+          type: Date,
+          default: Date.now,
+        },
+        deleted: { type: Boolean, default: false },
+        delete_password: String,
+        reported: { type: Boolean, default: false },
+      },
+    ],
+  });
+
+  /////////////////// Models
+  const Thread = mongoose.model('Stock', threadSchema);
+
+  /////////////////// Routes
+  app
+    .route('/api/threads/:board')
+    .post(async function (req, res) {
+      const board = req.params.board;
+      const { text, delete_password } = req.body;
+
+      const doc = await Thread.create({ board, text, delete_password });
+
+      res.status(201).json({
+        // 201 created
+        status: 'success',
+        data: {
+          data: doc,
+        },
+      });
+    })
+    .get(async function (req, res) {
+      const board = req.params.board;
+
+      const doc = await Thread.find({ board })
+        .limit(10)
+        .sort('-bumped_on')
+        .select('-replies.delete_password -replies.reported')
+        .where('replies')
+        .slice(3);
+
+      return res.json({ doc });
+    })
+    .delete(async function (req, res, next) {
+      const { thread_id, delete_password } = req.body;
+
+      const del = await Thread.deleteOne({
+        _id: thread_id,
+        delete_password,
+      });
+
+      if (del.deletedCount === 1) {
+        return res.send('success');
+      } else {
+        return res.send('incorrect password');
+      }
+    })
+
+    .put(async function (req, res, next) {
+      const { thread_id } = req.body;
+
+      const filter = {
+        _id: thread_id,
+      };
+
+      const updatedObject = {
+        $set: { reported: true },
+      };
+
+      await Thread.updateOne(filter, updatedObject, (err, updatedObject) => {
+        if (!err && updatedObject) {
+          return res.send('success');
+        }
+      });
+    });
+
+  app
+    .route('/api/replies/:board')
+    .get(async function (req, res) {
+      const thread_id = req.query.thread_id;
+
+      const doc = await Thread.find({ _id: thread_id }).select(
+        '-replies.delete_password -replies.reported'
+      );
+      return res.json({ doc });
+    })
+    .post(async function (req, res) {
+      const { text, delete_password, thread_id } = req.body;
+
+      const doc = await Thread.findByIdAndUpdate(
+        { _id: thread_id },
+        {
+          $push: { replies: { text, delete_password } },
+          $set: { bumped_on: Date.now() },
+        }
+      );
+
+      res.status(201).json({
+        // 201 created
+        status: 'success',
+        data: {
+          data: doc,
+        },
+      });
+    })
+    .delete(async function (req, res, next) {
+      const { thread_id, reply_id, delete_password } = req.body;
+
+      const filter = {
+        _id: thread_id,
+        'replies._id': reply_id,
+        'replies.delete_password': delete_password,
+      };
+      const update = {
+        $set: { 'replies.$.deleted': true },
+      };
+
+      const updated = await Thread.updateOne(filter, update);
+      if (updated.n === 1) {
+        return res.send('success');
+      } else {
+        return res.send('incorrect password');
+      }
+    })
+    .put(async function (req, res, next) {
+      const { thread_id, reply_id } = req.body;
+
+      const filter = {
+        _id: thread_id,
+        'replies._id': reply_id,
+      };
+
+      const updatedObject = {
+        $set: { 'replies.$.reported': true },
+      };
+
+      await Thread.updateOne(filter, updatedObject, (err, updatedObject) => {
+        if (!err && updatedObject) {
+          return res.send('success');
+        }
+      });
+    });
 };
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -5,6 +5,150 @@ const server = require('../server');
 
 chai.use(chaiHttp);
 
-suite('Functional Tests', function() {
+let board = 'testboard';
+let text = 'test text';
+let delete_password = 'deletepass';
+let thread_id;
+let reply_id;
 
+suite('Functional Tests', function () {
+  suite('API requests', () => {
+    test('Creating a new thread: POST request to /api/threads/{board}', function (done) {
+      chai
+        .request(server)
+        .post(`/api/threads/${board}`)
+        .send({
+          text,
+          delete_password,
+        })
+        .end(function (err, res) {
+          assert.equal(res.body.status, 'success');
+          done();
+        });
+    });
+
+    test('Viewing the 10 most recent threads with 3 replies each: GET request to /api/threads/{board}', function (done) {
+      chai
+        .request(server)
+        .get(`/api/threads/${board}`)
+        .end(function (err, res) {
+          assert.isArray(res.body.doc);
+          thread_id = res.body.doc[0]._id;
+          done();
+        });
+    });
+
+    test('Reporting a thread: PUT request to /api/threads/{board}', function (done) {
+      chai
+        .request(server)
+        .put(`/api/threads/${board}`)
+        .send({
+          thread_id,
+        })
+        .end(function (err, res) {
+          assert.equal(res.text, 'success');
+          done();
+        });
+    });
+    test('Creating a new reply: POST request to /api/replies/{board}', function (done) {
+      chai
+        .request(server)
+        .post(`/api/replies/${board}`)
+        .send({
+          text,
+          delete_password,
+          thread_id,
+        })
+        .end(function (err, res) {
+          assert.equal(res.body.status, 'success');
+          done();
+        });
+    });
+
+    test('Viewing a single thread with all replies: GET request to /api/replies/{board}', function (done) {
+      chai
+        .request(server)
+        .get(`/api/replies/${board}`)
+        .query({
+          thread_id,
+        })
+        .end(function (err, res) {
+          assert.equal(res.body.doc[0]._id, thread_id);
+          reply_id = res.body.doc[0].replies[0]._id;
+          done();
+        });
+    });
+
+    test('Reporting a reply: PUT request to /api/replies/{board}', function (done) {
+      chai
+        .request(server)
+        .put(`/api/replies/${board}`)
+        .send({
+          thread_id,
+          reply_id,
+        })
+        .end(function (err, res) {
+          assert.equal(res.text, 'success');
+          done();
+        });
+    });
+
+    test('Deleting a reply with the incorrect password: DELETE request to /api/threads/{board} with an invalid delete_password', function (done) {
+      chai
+        .request(server)
+        .delete(`/api/replies/${board}`)
+        .send({
+          thread_id,
+          reply_id,
+          delete_password: 'wrong',
+        })
+        .end(function (err, res) {
+          assert.equal(res.text, 'incorrect password');
+          done();
+        });
+    });
+
+    test('Deleting a reply with the correct password: DELETE request to /api/threads/{board} with a valid delete_password', function (done) {
+      chai
+        .request(server)
+        .delete(`/api/replies/${board}`)
+        .send({
+          thread_id,
+          reply_id,
+          delete_password,
+        })
+        .end(function (err, res) {
+          assert.equal(res.text, 'success');
+          done();
+        });
+    });
+
+    test('Deleting a thread with the incorrect password: DELETE request to /api/threads/{board} with an invalid delete_password', function (done) {
+      chai
+        .request(server)
+        .delete(`/api/threads/${board}`)
+        .send({
+          thread_id,
+          delete_password: 'wrong',
+        })
+        .end(function (err, res) {
+          assert.equal(res.text, 'incorrect password');
+          done();
+        });
+    });
+
+    test('Deleting a thread with the correct password: DELETE request to /api/threads/{board} with a valid delete_password', function (done) {
+      chai
+        .request(server)
+        .delete(`/api/threads/${board}`)
+        .send({
+          thread_id,
+          delete_password,
+        })
+        .end(function (err, res) {
+          assert.equal(res.text, 'success');
+          done();
+        });
+    });
+  });
 });
